be06db78e7abfeca50963a210af02a0308cfde0b
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2024-04-18 21:08:19 +0300

    Education entity created

---
+++ Entity/Education.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entity;
+
+public class Education
+{
+    public int EducationId { get; set; }
+    public int ExamId { get; set; }
+    public string EducationName { get; set; }
+    public string EducationDescription { get; set; }
+    public string VideoUrl { get; set; }
+    public string ContentImageUrl { get; set; }
+}
