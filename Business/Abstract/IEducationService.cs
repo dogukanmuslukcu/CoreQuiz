cf8633c946a58146203345fd82366e25cd690e62
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2024-04-18 21:52:29 +0300

    Education service module created

---
+++ Business/Abstract/IEducationService.cs
@@ -0,0 +1,19 @@
+﻿using Core.Utilities;
+using Core.Utilities.Abstract;
+using Core.Utilities.Concrete;
+using Entity;
+using Entity.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract;
+
+public interface IEducationService
+{
+    IDataResult <List<EducationDto>> GetAll();
+    IDataResult<List<EducationDto>> GetByExamId(int id);
+    IResult GetById(int id);
+}
---
+++ Business/Concrete/EducationManager.cs
@@ -0,0 +1,40 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Core.Utilities;
+using Core.Utilities.Abstract;
+using Core.Utilities.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using Entity;
+using Entity.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete;
+public class EducationManager : IEducationService
+{
+    private readonly IEducationDal _educationDal;
+
+    public EducationManager(IEducationDal educationDal)
+    {
+        _educationDal = educationDal;
+    }
+
+    public IDataResult<List<EducationDto>> GetAll()
+    {
+       return new SuccessDataResult<List<EducationDto>>(_educationDal.GetAll(), Messages.SuccessDataMessage);
+    }
+
+    public IDataResult<List<EducationDto>> GetByExamId(int id)
+    {
+        return new SuccessDataResult<List<EducationDto>>(_educationDal.GetByExamId(id), Messages.SuccessDataMessage);
+    }
+
+    public IResult GetById(int id)
+    {
+        return new SuccessDataResult<EducationDto>(_educationDal.GetById(id), Messages.SuccessMessage);
+    }
+}
